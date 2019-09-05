810ec39191f07fb4ab6e01a09af3cfb6207cf704
BornIncompetence <christopher_greer@elcamino.edu>
2019-09-05 10:50:24 -0700

    Fixed one JsonService function.

--- src/main/java/com/cecs/CreateAccountPage.java
+++ src/main/java/com/cecs/CreateAccountPage.java
@@ -1,22 +1,32 @@
 package com.cecs;
 
+import java.io.IOException;
+
 import io.reactivex.Flowable;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 class CreateAccountPage {
+    enum RegisterCode {
+        SUCCESS, INVALID_USER, INVALID_PASS1, INVALID_PASS2, NO_MATCH
+    }
+
     static void showAndWait(Stage parentStage) {
+        var stage = new Stage();
         var signUp = new Text("Sign Up");
         signUp.setFont(new Font(null, 36.0));
 
@@ -39,36 +49,80 @@ class CreateAccountPage {
         entries.setAlignment(Pos.CENTER);
         entries.maxWidth(225.0);
 
+        var errorMessage = new Label("");
+        errorMessage.setTextFill(Color.color(1.0, 0.2, 0.2));
+
         var registerButton = new Button("Register");
         registerButton.setOnAction(action -> {
             var u = userField.getText();
             var p1 = passField.getText();
-            var p2 = passField.getText();
+            var p2 = rePassField.getText();
             Flowable.fromCallable(() -> register(u, p1, p2)).subscribe(code -> {
-                System.out.println("Success path: " + code);
-                // TODO: After call is successful, do stuff
-            }, error -> {
-                System.out.println("Error path: " + error.getMessage());
-                // Path reached when register throws
-            });
+                switch (code) {
+                case SUCCESS: {
+                    var alert = new Alert(AlertType.INFORMATION);
+                    alert.setTitle("Confirmation");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Registration successful!");
+                    alert.showAndWait();
+
+                    stage.close();
+                    break;
+                }
+                case INVALID_USER: {
+                    errorMessage.setText("Username cannot be blank");
+                    break;
+                }
+                case INVALID_PASS1: {
+                    errorMessage.setText("Password cannot be blank");
+                    break;
+                }
+                case INVALID_PASS2: {
+                    errorMessage.setText("Retype Password cannot be blank");
+                    break;
+                }
+                case NO_MATCH: {
+                    errorMessage.setText("Passwords do not match");
+                    break;
+                }
+                }
+            }, Throwable::printStackTrace);
         });
 
-        var col = new VBox(signUp, entries, registerButton);
+        var createAccRow = new HBox(errorMessage, registerButton);
+        createAccRow.setAlignment(Pos.CENTER);
+
+        var col = new VBox(signUp, entries, createAccRow);
         col.setSpacing(10.0);
         col.setAlignment(Pos.CENTER);
         col.setPadding(new Insets(25.0));
 
-        var stage = new Stage();
         stage.setScene(new Scene(col, 320, 400));
         stage.initOwner(parentStage);
         stage.initModality(Modality.APPLICATION_MODAL);
         stage.showAndWait();
     }
 
-    private static boolean register(String user, String pass1, String pass2) {
-        if (user.isBlank() || pass1.isBlank() || pass2.isBlank()) {
-            throw new IllegalArgumentException("Cannot leave fields blank!");
+    /**
+     * Registers a user by inserting new data into a JSON file.
+     * @return `true` if neither the fields are blank and the password fields match,
+     *         `false` otherwise
+     * @throws IOException if the JsonService input stream fails, and other errors otherwise.
+     */
+    private static RegisterCode register(String user, String pass1, String pass2) throws IOException {
+        if (user.isBlank()) {
+            return RegisterCode.INVALID_USER;
         }
-        return true;
+        if (pass1.isBlank()) {
+            return RegisterCode.INVALID_PASS1;
+        }
+        if (pass2.isBlank()) {
+            return RegisterCode.INVALID_PASS2;
+        }
+        if (!pass1.equals(pass2)) {
+            return RegisterCode.NO_MATCH;
+        }
+        JsonService.createAccount(user, pass1);
+        return RegisterCode.SUCCESS;
     }
 }
--- src/main/java/com/cecs/JsonService.java
+++ src/main/java/com/cecs/JsonService.java
@@ -4,45 +4,40 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonIOException;
 import com.google.gson.JsonSyntaxException;
-import com.google.gson.reflect.TypeToken;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.Arrays;
 
-public class JsonService {
-
+class JsonService {
     /*
-    Function to create a new User and add the User to the users json file
-    */
-    public static void CreateAccount(String user, String pass){
-        User newUser = new User(user, pass);
+     * Function to create a new User and add the User to the users json file
+     */
+    static void createAccount(String user, String pass) throws IOException {
+        var newUser = new User(user, pass);
+        var gson = new GsonBuilder().setPrettyPrinting().create();
 
-        Gson gson = new Gson();
+        // Load previous Users from user file
+        var reader = new FileReader("users.json", StandardCharsets.UTF_8);
+        var users = gson.fromJson(reader, User[].class);
+        var newUsers = new User[users.length + 1];
 
-        try {
-            var reader = new InputStreamReader(MainPage.class.getResourceAsStream("/users.json"),
-                    StandardCharsets.UTF_8);
-            User[] users = new GsonBuilder().create().fromJson(reader, User[].class); // Read users from users.json file
+        // Append new User to old User list
+        System.arraycopy(users, 0, newUsers, 0, users.length);
+        newUsers[users.length] = newUser;
 
-            User[] addedUsers = new User[users.length + 1]; // Create a new array for the new user
+        // Create string from array of Users and write to file
+        var jsonUsers = gson.toJson(newUsers);
+        var writer = new FileWriter("users.json");
+        writer.write(jsonUsers);
+        writer.close();
+    }
 
-            addedUsers[users.length] = newUser;
-
-            String jsonUsers = gson.toJson(addedUsers);
-
-            FileWriter writer = new FileWriter("users.json"); // Write the new array of users to file
-            writer.write(jsonUsers);
-            writer.close();
-
-        } catch (NullPointerException e) {
-            System.err.println("Instantiating input stream failed.");
-        } catch (JsonSyntaxException | JsonIOException e) {
-            System.err.println("Could not populate music list.");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    /**
+     * Searches for user, and if found, returns serialized object
+     */
+    static User login(String user, String pass) {
+        // TODO: Everything
+        return new User("a", "b");
     }
 }
