a03e730d1030cb03063e50629ffc98af1dd43340
BornIncompetence <christopher_greer@elcamino.edu>
2019-09-17 11:39:20 -0700

    Polished UI
    Bug that caused compiler failure has been fixed, and the song slider now works again. Menus polished.

--- src/main/java/com/cecs/MainPage.java
+++ src/main/java/com/cecs/MainPage.java
@@ -20,72 +20,50 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.scene.control.Slider;
 import javafx.scene.control.MenuBar;
-
+import javafx.scene.control.MenuItem;
 
 class MainPage {
     static void show(Stage stage, User user) {
-
-
-        Menu MainMenu = new Menu("All Songs");
-        Menu PlaylistMenu = new Menu("Playlists");
-        Menu ProfileMenu = new Menu("User Profile");
-        Menu SettingsMenu = new Menu("Settings");
-
-        //Main Menu
-        Button mainpageButton = new Button("View All");
-        CustomMenuItem customMenuItem_Main = new CustomMenuItem();
-        customMenuItem_Main.setContent(mainpageButton);
-        customMenuItem_Main.setHideOnClick(false);
-        MainMenu.getItems().add(customMenuItem_Main);
-
-        //Main Menu
-        Button PlaylistMenuButton = new Button("View All");
-        CustomMenuItem playlistItem = new CustomMenuItem();
-        playlistItem.setContent(PlaylistMenuButton);
-        playlistItem.setHideOnClick(false);
-        PlaylistMenu.getItems().add(playlistItem);
-
-
-        //Settings Menu Items
-        Slider slider = new Slider(0, 100, 50);
-        CustomMenuItem customMenuItem = new CustomMenuItem();
-        customMenuItem.setContent(slider);
-        customMenuItem.setHideOnClick(false);
-        SettingsMenu.getItems().add(customMenuItem);
-
-        Button customButton = new Button("Custom Menu Item Button");
-        CustomMenuItem customMenuItem2 = new CustomMenuItem();
-        customMenuItem2.setContent(customButton);
-        customMenuItem2.setHideOnClick(false);
-        SettingsMenu.getItems().add(customMenuItem2);
-
-
-
-
-
-
-        MainMenu.getItems().add(customMenuItem_Main);
-
-
-        MenuBar menuBar = new MenuBar();
-        menuBar.getMenus().add(MainMenu);
-        menuBar.getMenus().add(PlaylistMenu);
-        menuBar.getMenus().add(ProfileMenu);
-        menuBar.getMenus().add(SettingsMenu);
-
-        menuBar.prefWidthProperty().bind(stage.widthProperty());
-
-
-
-
-
-
+        // Initialize Song Player service
         SongPlayer player = new SongPlayer();
+
+        // Main Menu
+        var viewAll = new MenuItem("View All");
+        viewAll.setOnAction(action -> {
+            System.out.println("View all selected");
+            // TODO: Functionality, use your imagination.
+        });
+        var mainMenu = new Menu("All Songs", null, viewAll);
+
+        // Playlist Menu
+        var playlistItem = new MenuItem("Go to Playlists");
+        playlistItem.setOnAction(action -> {
+            PlaylistPage.show(stage, user);
+        });
+        var playlistMenu = new Menu("Playlists", null, playlistItem);
+
+        // Profile Menu
+        var profileMenu = new Menu("User Profile");
+
+        // Settings Menu Items
+        var menuSlider = new Slider(0, 100, 50);
+        var customMenuItem = new CustomMenuItem();
+        customMenuItem.setContent(menuSlider);
+        customMenuItem.setHideOnClick(false);
+        var otherSettingItem = new MenuItem("Other Settings Item");
+        otherSettingItem.setOnAction(action -> {
+            System.out.println("Other setting selected");
+            // TODO: Functionality, use your imagination.
+        });
+        var settingsMenu = new Menu("Settings", null, customMenuItem, otherSettingItem);
+
+        // Menu Bar
+        var menuBar = new MenuBar(mainMenu, playlistMenu, profileMenu, settingsMenu);
 
         var listOfMusic = FXCollections.<Music>observableArrayList();
         Flowable.fromCallable(JsonService::loadDatabase).subscribe(listOfMusic::addAll, Throwable::printStackTrace);
 
-        final var label = new Text("Welcome back, " + user.username);
+        var label = new Text("Welcome back, " + user.username);
         label.setFont(Font.font(null, FontPosture.ITALIC, 24.0));
 
         var songs = new TableColumn<Music, String>("Song");
@@ -111,6 +89,7 @@ class MainPage {
             });
         });
 
+        // Track slider, controls when to stop/continue track updates
         var playbackSlider = new Slider();
         playbackSlider.setDisable(true);
         playbackSlider.setOnMouseReleased(it -> {
@@ -158,6 +137,7 @@ class MainPage {
             playButton.setText("⏸");
         });
 
+        // Convoluted logic for determining when a play previous/next is warranted
         table.getSelectionModel().selectedItemProperty().addListener((_0, _1, newSelection) -> {
             if (newSelection == null) {
                 prevSongButton.setDisable(true);
@@ -166,7 +146,7 @@ class MainPage {
             } else {
                 var currentIndex = table.getSelectionModel().getSelectedIndex();
                 var prevDisabled = currentIndex == 0;
-                var nextDisabled = currentIndex == table.getItems().size() - 1;
+                var nextDisabled = currentIndex == table.getItems().size() - 1; // Not tested yet
 
                 if (player.nowPlaying() != null) {
                     if (!player.nowPlaying().equals(newSelection.getSong().getId() + ".mp3")) {
@@ -189,30 +169,18 @@ class MainPage {
         controlButtonRow.setLeft(prevSongButton);
         controlButtonRow.setCenter(playButton);
         controlButtonRow.setRight(nextSongButton);
-        controlButtonRow.setMaxWidth(750.0);
+        controlButtonRow.setPadding(new Insets(20.0));
 
-        VBox vbox = new VBox();
+        var vbox = new VBox(label, searchBar, table, playbackSlider);
         vbox.setSpacing(10);
-        vbox.setPadding(new Insets(25,25,25,25));
-        vbox.getChildren().add(label);
-        vbox.getChildren().add(searchBar);
-        vbox.getChildren().add(table);
-        vbox.getChildren().add(playbackSlider);
+        vbox.setPadding(new Insets(25.0));
 
-//        final var musicRow = new vBox(label, searchBar, table, playbackSlider);
-//        musicRow.setSpacing(10.0);
-//        musicRow.setPadding(new Insets(10.0));
-
-        BorderPane menuPane = new BorderPane();
-        menuPane.setTop(menuBar);
-
-
-        //final var col = new VBox(menuPane, label, searchBar, table, playbackSlider, controlButtonRow);
-        final var col = new VBox(menuPane, vbox, controlButtonRow);
-        col.setSpacing(10.0);
-        //col.setPadding(new Insets(25.0));
-        final var scene = new Scene(col, 800, 600);
-
+        final var borderPane = new BorderPane();
+        borderPane.setCenter(vbox);
+        borderPane.setTop(menuBar);
+        borderPane.setBottom(controlButtonRow);
+        // col.setPadding(new Insets(25.0));
+        final var scene = new Scene(borderPane, 800, 600);
 
         stage.setScene(scene);
         stage.show();
