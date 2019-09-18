86a85b4455039625d51edd9b0be5a6140b858a20
BornIncompetence <christopher_greer@elcamino.edu>
2019-09-18 13:46:37 -0700

    Added media controls to Playlist Page
    With this commit, the MyPlaylistPage is now feature complete! Also reorganized the UI to be more consistent between pages

--- src/main/java/com/cecs/MainPage.java
+++ src/main/java/com/cecs/MainPage.java
@@ -2,19 +2,16 @@ package com.cecs;
 
 import io.reactivex.Flowable;
 import javafx.beans.binding.Bindings;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
 import javafx.event.ActionEvent;
 import javafx.geometry.Insets;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
@@ -63,56 +60,26 @@ class MainPage {
         // Menu Bar
         var menuBar = new MenuBar(mainMenu, playlistMenu, profileMenu, settingsMenu);
 
-
-        /*Menu MainMenu = new Menu("All Songs");
-        Menu PlaylistMenu = new Menu("Playlists");
-        Menu ProfileMenu = new Menu("User Profile");
-        Menu SettingsMenu = new Menu("Settings");
-
-        //Main Menu
-        Button mainpageButton = new Button("View All");
-        CustomMenuItem customMenuItem_Main = new CustomMenuItem();
-        customMenuItem_Main.setContent(mainpageButton);
-        customMenuItem_Main.setHideOnClick(false);
-        MainMenu.getItems().add(customMenuItem_Main);
-
-        //Main Menu
-        Button PlaylistMenuButton = new Button("View All");
-        CustomMenuItem playlistItem = new CustomMenuItem();
-        playlistItem.setContent(PlaylistMenuButton);
-        playlistItem.setHideOnClick(false);
-        PlaylistMenu.getItems().add(playlistItem);
-
-
-        //Settings Menu Items
-        Slider slider = new Slider(0, 100, 50);
-        CustomMenuItem customMenuItem = new CustomMenuItem();
-        customMenuItem.setContent(slider);
-        customMenuItem.setHideOnClick(false);
-        SettingsMenu.getItems().add(customMenuItem);
-
-        Button customButton = new Button("Custom Menu Item Button");
-        CustomMenuItem customMenuItem2 = new CustomMenuItem();
-        customMenuItem2.setContent(customButton);
-        customMenuItem2.setHideOnClick(false);
-        SettingsMenu.getItems().add(customMenuItem2);
-
-        MainMenu.getItems().add(customMenuItem_Main);
-
-        MenuBar menuBar = new MenuBar();
-        menuBar.getMenus().add(MainMenu);
-        menuBar.getMenus().add(PlaylistMenu);
-        menuBar.getMenus().add(ProfileMenu);
-        menuBar.getMenus().add(SettingsMenu);
-
-        menuBar.prefWidthProperty().bind(stage.widthProperty());*/
-
         var listOfMusic = FXCollections.<Music>observableArrayList();
         Flowable.fromCallable(JsonService::loadDatabase).subscribe(listOfMusic::addAll, Throwable::printStackTrace);
         final var label = new Text("Welcome back, " + user.username);
         label.setFont(Font.font(null, FontPosture.ITALIC, 24.0));
 
-        var myPlaylist = new Button("My Playlist");
+        // Go to MyPlaylistPage
+        var myPlaylistButton = new Button("My Playlists");
+        myPlaylistButton.setOnAction(e -> MyPlaylistPage.show(stage, player, user));
+
+        var obv = FXCollections.<String>observableArrayList();
+        // var map = new HashMap<String, Playlist>();
+
+        for (Playlist pl : user.getUserPlaylists()) {
+            obv.add(pl.getName());
+            // map.put(pl.getName(), pl);
+        }
+
+        var cbMyPlaylist = new ComboBox<>(obv);
+        cbMyPlaylist.setEditable(true);
+        cbMyPlaylist.setValue((obv.isEmpty()) ? "" : obv.get(0));
 
         var songs = new TableColumn<Music, String>("Song");
         songs.setCellValueFactory(new PropertyValueFactory<>("song"));
@@ -120,79 +87,47 @@ class MainPage {
         releases.setCellValueFactory(new PropertyValueFactory<>("release"));
         var artists = new TableColumn<Music, String>("Artist");
         artists.setCellValueFactory(new PropertyValueFactory<>("artist"));
+        var colBtn = new TableColumn<Music, Void>("Add to Playlist");
+        colBtn.setCellFactory((TableColumn<Music, Void> features) -> new TableCell<>() {
+            // Create Add Playlist button
+            private final Button btn = new Button("Add");
+            {
+                btn.setOnAction((ActionEvent event) -> {
+                    Music music = getTableView().getItems().get(getIndex());
+                    Song song = music.getSong();
+                    String plName = cbMyPlaylist.getValue();
+                    if (user.containsPlaylist(plName)) {
+                        for (Playlist pl : user.getUserPlaylists()) {
+                            if (pl.getName().equals(plName))
+                                pl.addSong(song);
+                        }
+                    } else {
+                        Playlist pl = new Playlist(plName, new ArrayList<>(List.of(song)));
+                        user.getUserPlaylists().add(pl);
+                        obv.add(plName);
+                        cbMyPlaylist.getItems().add(plName);
+                    }
+                    try {
+                        JsonService.updateUser(user);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+            }
+
+            @Override
+            public void updateItem(Void item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty) {
+                    setGraphic(null);
+                } else {
+                    setGraphic(btn);
+                }
+            }
+        });
 
         var list = new FilteredList<>(listOfMusic, m -> true);
         var table = new TableView<>(list);
-
-        TableColumn<Music, Void> colBtn = new TableColumn("Add to Playlist");
-
-        List<String> pls = new ArrayList<>();
-        Map<String, Playlist> map = new HashMap<>();
-
-        for(Playlist pl: user.getUserPlaylists()){
-            pls.add(pl.getName());
-            map.put(pl.getName(), pl);
-
-        }
-
-        //ObservableList<String> playlists = FXCollections.observableArrayList(pls);
-        ComboBox cbMyPlaylist = new ComboBox<>();
-        cbMyPlaylist.getItems().addAll(pls);
-
-        cbMyPlaylist.setEditable(true);
-        cbMyPlaylist.setValue((pls.isEmpty()) ? "" : pls.get(0));
-
-        // Create Add Playlist button
-        Callback<TableColumn<Music, Void>, TableCell<Music, Void>> btnCellFact = new Callback<TableColumn<Music, Void>, TableCell<Music, Void>>() {
-            @Override
-            public TableCell<Music, Void> call(final TableColumn<Music, Void> param) {
-                final TableCell<Music, Void> cell = new TableCell<Music, Void>() {
-
-                    private final Button btn = new Button("Add");
-                    {
-                        btn.setOnAction((ActionEvent event) -> {
-                            Music music = getTableView().getItems().get(getIndex());
-                            Song song =  music.getSong();
-                            String plName = (String) cbMyPlaylist.getValue();
-                            if(user.containsPlaylist(plName)){
-                                for(Playlist pl : user.getUserPlaylists()){
-                                    if(pl.getName().equals(plName))
-                                        pl.addSong(song);
-                                }
-                            }
-                            else {
-                                Playlist pl = new Playlist();
-                                pl.setName(plName);
-                                pl.addSong(song);
-                                user.getUserPlaylists().add(pl);
-                                pls.add(plName);
-                                cbMyPlaylist.getItems().add(plName);
-                            }
-                            try {
-                                JsonService.updateUser(user);
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-
-                        });
-                    }
-
-                    @Override
-                    public void updateItem(Void item, boolean empty) {
-                        super.updateItem(item, empty);
-                        if (empty) {
-                            setGraphic(null);
-                        } else {
-                            setGraphic(btn);
-                        }
-                    }
-                };
-                return cell;
-            }
-        };
-
-        colBtn.setCellFactory(btnCellFact);
-
         table.setEditable(true);
         table.getColumns().addAll(songs, releases, artists, colBtn);
 
@@ -206,6 +141,11 @@ class MainPage {
                         || p.getSong().toString().toLowerCase().contains(query);
             });
         });
+        searchBar.prefWidthProperty().bind(Bindings.divide(stage.widthProperty(), 2));
+
+        // De-clutter bottom of track slider
+        var searchRow = new HBox(searchBar, myPlaylistButton, cbMyPlaylist);
+        searchRow.setSpacing(10.0);
 
         // Track slider, controls when to stop/continue track updates
         var playbackSlider = new Slider();
@@ -283,29 +223,23 @@ class MainPage {
             }
         });
 
+        var vbox = new VBox(label, searchRow, table, playbackSlider);
+        vbox.setSpacing(10.0);
+        vbox.setPadding(new Insets(25.0));
+
         var controlButtonRow = new BorderPane();
         controlButtonRow.setLeft(prevSongButton);
         controlButtonRow.setCenter(playButton);
         controlButtonRow.setRight(nextSongButton);
         controlButtonRow.setPadding(new Insets(20.0));
-        var headline = new HBox(label, myPlaylist);
-        headline.setSpacing(30.0);
-        var vbox = new VBox(label, searchBar, table, playbackSlider);
-        vbox.getChildren().add(headline);
-        vbox.getChildren().add(cbMyPlaylist);
-        vbox.setSpacing(10);
-        vbox.setPadding(new Insets(25.0));
 
         final var borderPane = new BorderPane();
-        borderPane.setCenter(vbox);
         borderPane.setTop(menuBar);
+        borderPane.setCenter(vbox);
         borderPane.setBottom(controlButtonRow);
         // col.setPadding(new Insets(25.0));
-        // Go to MyPlaylistPage
-        myPlaylist.setOnAction(e -> MyPlaylistPage.show(stage, player, user));
         final var scene = new Scene(borderPane, 800, 600);
 
-        
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/cecs/MyPlaylistPage.java
+++ src/main/java/com/cecs/MyPlaylistPage.java
@@ -6,6 +6,7 @@ import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
@@ -19,6 +20,11 @@ import java.util.Map;
 
 class MyPlaylistPage {
     static void show(Stage stage, SongPlayer player, User user) {
+        var label = new Text("Playlists of " + user.username);
+        label.setFont(Font.font(null, FontPosture.ITALIC, 24.0));
+
+        var btMainPage = new Button("Search songs");
+        btMainPage.setOnAction(e -> MainPage.show(stage, player, user));
 
         ObservableList<Song> songs = FXCollections.observableArrayList();
         if (user.getUserPlaylists().size() > 0) {
@@ -35,16 +41,8 @@ class MyPlaylistPage {
         artistCol.setCellValueFactory(new PropertyValueFactory<>("artist"));
 
         TableView<Song> table = new TableView<>(songs);
-
         table.setEditable(true);
         table.getColumns().addAll(titleCol, artistCol);
-
-        final var label = new Text("Playlists of " + user.username);
-        var btMainPage = new Button("Search songs");
-        btMainPage.setOnAction(e -> MainPage.show(stage, player, user));
-        label.setFont(Font.font(null, FontPosture.ITALIC, 24.0));
-        var line = new HBox(label, btMainPage);
-        line.setSpacing(30.0);
 
         var obv = FXCollections.<String>observableArrayList();
         // var map = new HashMap<String, Playlist>();
@@ -55,7 +53,6 @@ class MyPlaylistPage {
         }
 
         var cbMyPlaylist = new ComboBox<>(obv);
-
         cbMyPlaylist.setValue((obv.isEmpty()) ? "" : obv.get(0));
         cbMyPlaylist.setOnAction(e -> {
             for (Playlist pl : user.getUserPlaylists()) {
@@ -66,7 +63,90 @@ class MyPlaylistPage {
             table.setItems(songs);
         });
 
-        final var col = new VBox(line, cbMyPlaylist, table);
+        var line = new HBox(cbMyPlaylist, btMainPage);
+        line.setSpacing(10.0);
+
+        // Track slider, controls when to stop/continue track updates
+        var playbackSlider = new Slider();
+        playbackSlider.setDisable(true);
+        playbackSlider.setOnMouseReleased(it -> {
+            player.unblockUpdates();
+            player.updateTrack(playbackSlider.getValue());
+        });
+        playbackSlider.setOnMouseDragged(it -> {
+            player.blockUpdates();
+        });
+        player.getEvents().subscribe(playbackSlider::setValue, Throwable::printStackTrace);
+
+        var playButton = new Button("▶");
+        playButton.setDisable(true);
+        playButton.setOnAction(action -> {
+            if (playButton.getText().equals("▶")) { // lol
+                var song = table.getSelectionModel().getSelectedItem();
+                stage.setTitle("Music Player 1.0" + " - Now Playing: " + song.getTitle());
+                player.playSong(song.getId() + ".mp3");
+                playButton.setText("⏸");
+                playbackSlider.setDisable(false);
+            } else {
+                player.pauseSong();
+                stage.setTitle("Music Player 1.0");
+                playButton.setText("▶");
+            }
+        });
+
+        var prevSongButton = new Button("⏮");
+        prevSongButton.setDisable(true);
+        prevSongButton.setOnAction(action -> {
+            table.getSelectionModel().selectPrevious();
+            var song = table.getSelectionModel().getSelectedItem();
+            stage.setTitle("Music Player 1.0" + " - Now Playing: " + song.getTitle());
+            player.playSong(song.getId() + ".mp3");
+            playButton.setText("⏸");
+        });
+
+        var nextSongButton = new Button("⏭");
+        nextSongButton.setDisable(true);
+        nextSongButton.setOnAction(action -> {
+            table.getSelectionModel().selectNext();
+            var song = table.getSelectionModel().getSelectedItem();
+            stage.setTitle("Music Player 1.0" + " - Now Playing: " + song.getTitle());
+            player.playSong(song.getId() + ".mp3");
+            playButton.setText("⏸");
+        });
+
+        table.getSelectionModel().selectedItemProperty().addListener((_0, _1, newSelection) -> {
+            if (newSelection == null) {
+                prevSongButton.setDisable(true);
+                playButton.setDisable(true);
+                nextSongButton.setDisable(true);
+            } else {
+                var currentIndex = table.getSelectionModel().getSelectedIndex();
+                var prevDisabled = currentIndex == 0;
+                var nextDisabled = currentIndex == table.getItems().size() - 1; // Not tested yet
+
+                if (player.nowPlaying() != null) {
+                    if (!player.nowPlaying().equals(newSelection.getId() + ".mp3")) {
+                        playButton.setText("▶");
+                    } else {
+                        playButton.setText("⏸");
+                    }
+                } else {
+                    prevDisabled = true;
+                    nextDisabled = true;
+                }
+
+                prevSongButton.setDisable(prevDisabled);
+                playButton.setDisable(false);
+                nextSongButton.setDisable(nextDisabled);
+            }
+        });
+
+        var controlButtonRow = new BorderPane();
+        controlButtonRow.setLeft(prevSongButton);
+        controlButtonRow.setCenter(playButton);
+        controlButtonRow.setRight(nextSongButton);
+
+        final var col = new VBox(label, line, table, playbackSlider, controlButtonRow);
         col.setSpacing(10.0);
         col.setPadding(new Insets(25.0));
         final var scene = new Scene(col, 800, 600);
