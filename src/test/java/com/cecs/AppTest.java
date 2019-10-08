e2993d90510252bf0ecd508be26dc31f8789546b
BornIncompetence <christopher_greer@elcamino.edu>
2019-10-08 09:39:30 -0700

    Start to create test for login server

--- src/test/java/com/cecs/AppTest.java
+++ src/test/java/com/cecs/AppTest.java
@@ -8,23 +8,27 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AppTest {
-    private final String jason =
-    "{ " +
-    "\"remoteMethod\": \"getSongChunk\", " +
-    "\"objectName\": \"SongServices\", " +
-    "\"param\": { " +
-    "\"song\": \"SOMZWCG12A8C13C480\", \"fragment\": 2 " +
-    "} " +
-    "}";
+    private final String jasonBuffer = "{ \"remoteMethod\": \"getSongChunk\", \"objectName\": \"SongServices\", \"param\": { \"song\": \"SOMZWCG12A8C13C480\", \"fragment\": 2 } }";
+    private final String jasonLogin = "{ \"remoteMethod\": \"login\", \"objectName\": \"UserServices\", \"param\": { \"username\": \"user\", \"password\": \"pass\" } }";
 
     @Test
     void testDispatch() {
         var dispatch = new Dispatcher();
-        var ret = dispatch.dispatch(jason);
+        var ret = dispatch.dispatch(jasonBuffer);
         System.out.println("Packet: " + ret);
         var bytes = JsonService.unpackBytes(ret);
 
         assertEquals(8192, bytes.length);
+    }
+
+    @Test
+    void testLogin() {
+        var dispatch = new Dispatcher();
+        var ret = dispatch.dispatch(jasonLogin);
+        System.out.println("Packet: " + ret);
+        var bytes = JsonService.unpackBytes(ret);
+
+        assertEquals(bytes.toString(), "Authentication successful");
     }
 
     @AfterAll
