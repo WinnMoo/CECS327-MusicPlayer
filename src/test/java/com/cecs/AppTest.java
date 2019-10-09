c47f436810cb7d177246086f5b40d707db2104d8
BornIncompetence <christopher_greer@elcamino.edu>
2019-10-09 11:05:57 -0700

    Add test cases for logging in user
    Also resolved a breaking change that occured in the server in order for it to handle custom types gracefully.

--- src/test/java/com/cecs/AppTest.java
+++ src/test/java/com/cecs/AppTest.java
@@ -5,11 +5,13 @@ import com.cecs.controller.JsonService;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AppTest {
     private final String jasonBuffer = "{ \"remoteMethod\": \"getSongChunk\", \"objectName\": \"SongServices\", \"param\": { \"song\": \"SOMZWCG12A8C13C480\", \"fragment\": 2 } }";
-    private final String jasonLogin = "{ \"remoteMethod\": \"login\", \"objectName\": \"UserServices\", \"param\": { \"username\": \"user\", \"password\": \"pass\" } }";
+    private final String jasonLogin = "{ \"remoteMethod\": \"login\", \"objectName\": \"UserServices\", \"param\": { \"username\": \"chris\", \"password\": \"greer\" } }";
 
     @Test
     void testDispatch() {
@@ -25,10 +27,17 @@ class AppTest {
     void testLogin() {
         var dispatch = new Dispatcher();
         var ret = dispatch.dispatch(jasonLogin);
-        System.out.println("Packet: " + ret);
-        var bytes = JsonService.unpackBytes(ret);
+        var playlists = JsonService.unpackPlaylists(ret);
 
-        assertEquals(bytes.toString(), "Authentication successful");
+        if (playlists == null) {
+            System.out.println("This user doesn't exist");
+        } else if (playlists.length == 0) {
+            System.out.println("This user does not have any Playlists");
+        } else {
+            System.out.println("This user has " + playlists.length + " playlist(s)");
+        }
+
+        // System.out.println(Arrays.toString(bytes));
     }
 
     @AfterAll
